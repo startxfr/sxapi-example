a9ff5f8e6cc1b277e23760d00c52a1869c9c277c
startxfr <clarue@startx.fr>
2018-03-03 21:34:59 +0100

    remove code, leave config only

--- app.js
+++
@@ -1,6 +0,0 @@
-// Load the microservice stack
-
-var sxapi = require("sxapi-core");
-sxapi.app.launch(function () {
-    sxapi.app.log.info("application started", sxapi.app.timer.time('app'));
-});
---
+++ mylib.js
@@ -0,0 +1,12 @@
+/* global $log, module */
+
+//'use strict';
+
+var mylib = {
+  myCronFunction: function (config) {
+    var moment = require('moment');
+    $log.info("cron task " + config.id + " executed at " + moment().format('YYYY-MM-DD HH:mm:ss'));
+  }
+};
+
+module.exports = mylib;
